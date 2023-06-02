1b3414d2a6b78f1298e2cbc48569d77092a33b6b
Danya Generalov <danya@danya02.ru>
2023-06-02 17:03:35 +0300

    Add example mod template.

---
+++ src/main/kotlin/ru/danya02/mcmetrics/ExampleMod.kt
@@ -0,0 +1,15 @@
+package ru.danya02.mcmetrics
+
+import net.fabricmc.api.ModInitializer
+import org.slf4j.LoggerFactory
+
+object ExampleMod : ModInitializer {
+    private val logger = LoggerFactory.getLogger("server-metrics-exporter")
+
+	override fun onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+		logger.info("Hello Fabric world!")
+	}
+}
